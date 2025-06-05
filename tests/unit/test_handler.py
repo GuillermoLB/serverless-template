9121aaf76a320c7a1ecccfd9553a5bda05d5f72f
GuillermoLB <guillelb99@gmail.com>
2025-06-05 12:48:49 +0200

    Remove redundant assertions from lambda handler unit test

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -68,5 +68,3 @@ def test_lambda_handler(apigw_event):
     data = json.loads(ret["body"])
 
     assert ret["statusCode"] == 200
-    assert "message" in ret["body"]
-    assert data["message"] == "hello world"
